f624284af8bc9c1e72636e0eb9a0f98d814ee4b2
jera1470 <jeremycaceres40@gmail.com>
2026-02-07 11:17:47 -0600

    columns and projectcards made
    use placeholder images for now
    (nethack image and a default
    pfp image)
    
    fix navbar to appear on all
    pages, as well as its size

---
+++ frontend/src/components/ProjectCard.jsx
@@ -0,0 +1,22 @@
+import '../css/ProjectCard.css'
+import nethack from '../assets/nethack.png'
+import default2 from '../assets/Default_pfp.jpg'
+import {Link} from "react-router"
+
+function ProjectCard() {
+  return (
+    <div className="project-card">
+      <div className="project-details">
+        <img src={default2} className="project-pfp" alt="User Profile"/>
+        <div>
+          <Link to="/">Project Title</Link>
+        </div>
+      </div>
+      <div className="project-image">
+        <img src={nethack} alt="Project Screenshot"/>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectCard;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,52 @@
 import NavBar from '../components/NavBar.jsx'
+import '../css/Home.css'
+import ProjectCard from '../components/ProjectCard.jsx'
 
 function Home() {
   return (
     <div className="home">
+      <table
+        className="content-columns">
+        <tr className="content-columns-header">
+          <td>
+            <div className="header-text">
+              f
+            </div>
+          </td>
+          <td>
+            <div className="header-text">
+              f
+            </div>
+          </td>
+          <td className="tdlast">
+            <div className="header-text">
+              f
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div className="scroll-content">
+              <ProjectCard/>
+              <ProjectCard/>
+              <ProjectCard/>
+              <ProjectCard/>
+              <ProjectCard/>
+              <ProjectCard/>
+            </div>
+          </td>
+          <td>
+            <div className="scroll-content">
 
+            </div>
+          </td>
+          <td className="tdlast">
+            <div className="scroll-content">
+
+            </div>
+          </td>
+        </tr>
+      </table>
     </div>
   )
 }
